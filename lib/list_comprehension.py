8c5049836cf5f7e3f84fdaed06ff0f7d10224778
jameskimani001 <jameskim092t@gmail.com>
2024-11-28 10:31:43 +0300

    list comprehesion complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-
 def return_evens(num_list):
-    pass
+    # Use list comprehension to select only even numbers
+    return [n for n in num_list if n % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    # Use list comprehension to append "!" to each sentence
+    return [sentence + "!" for sentence in sentence_list]
